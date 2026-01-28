0fe978f4018acc855e15a5bbfcc5ce20347d344d
closerpilot <closerpilot@gmail.com>
2026-01-28 23:17:10 +0000

    Refractor Front-end

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { Routes, Route } from 'react-router'
 import HomePage from './pages/HomePage'
 import CheckoutPage from './pages/CheckoutPage'
+import OrderPages from './pages/OrdersPage'
+import TrackingPage from './pages/TrackingPage'
 import './App.css'
 
 function App() {
@@ -9,6 +11,8 @@ function App() {
     <Routes>
       <Route index element={<HomePage />}/>
       <Route path='checkout' element={<CheckoutPage/>}/>
+      <Route path='orders' element={<OrderPages/>}/>
+      <Route path='tracking' element={<TrackingPage/>}/>
    </Routes>
   )
 }
