b3979d382faaeeb33906d143667c35941080a1e7
closerpilot <closerpilot@gmail.com>
2026-02-05 00:00:15 +0000

    Future fix for links pending

---
+++ src/pages/index.jsx
@@ -0,0 +1,4 @@
+export { default as HomePage } from './HomePage'
+export { default as CheckoutPage } from './CheckoutPage'
+export { default as OrderPage } from './OrderPage'
+export { default as TrackingPage } from './TrackingPage'
