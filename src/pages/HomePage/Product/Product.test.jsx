592ae8d053218c75fb79e97d9d96fd8bcd415074
closerpilot <closerpilot@gmail.com>
2026-02-09 18:49:47 +0000

    Use beforeEach to remove duplication

--- src/pages/HomePage/Product/Product.test.jsx
+++ src/pages/HomePage/Product/Product.test.jsx
@@ -1,4 +1,4 @@
-import { it, expect, describe, vi } from 'vitest';
+import { it, expect, describe, vi, beforeEach } from 'vitest';
 import { render, screen } from '@testing-library/react';
 import { Product } from './Product.jsx';
 import { userEvent } from '@testing-library/user-event'
@@ -8,9 +8,13 @@ import { api } from '../../../api/api.jsx';
 
 vi.mock('../../../api/api.jsx')
 
-describe('Product Component',  () => {
-  it('displays the product details correctly', () => {
-    const product = {
+describe('Product Component', () => {
+
+  //Vuelve a cargar las variables para cada test case
+  let product;
+  let loadCart;
+  beforeEach(()=>{
+    product = {
       "id": "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
       "image": "images/products/athletic-cotton-socks-6-pairs.jpg",
       "name": "Black and Gray Athletic Cotton Socks - 6 Pairs",
@@ -22,7 +26,10 @@ describe('Product Component',  () => {
       "keywords": ["socks", "sports", "apparel"]
     }
 
-    const loadCart = vi.fn()
+    loadCart = vi.fn()
+  })
+
+  it('displays the product details correctly', () => {
     render(<Product product={product} loadCart={loadCart} />);
 
     expect(screen.getByText('Black and Gray Athletic Cotton Socks - 6 Pairs')).toBeInTheDocument();
@@ -33,19 +40,6 @@ describe('Product Component',  () => {
   })
 
   it('adds a product to the cart', async () => {
-    const product = {
-      "id": "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-      "image": "images/products/athletic-cotton-socks-6-pairs.jpg",
-      "name": "Black and Gray Athletic Cotton Socks - 6 Pairs",
-      "rating": {
-        "stars": 4.5,
-        "count": 87
-      },
-      "priceCents": 1090,
-      "keywords": ["socks", "sports", "apparel"]
-    }
-
-    const loadCart = vi.fn()
     render(<Product product={product} loadCart={loadCart} />);
 
     const user = userEvent.setup();
