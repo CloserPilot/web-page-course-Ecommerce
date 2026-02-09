f66ee561ed815bb45777b0665fa2f11a8ae2a5fc
closerpilot <closerpilot@gmail.com>
2026-02-09 18:45:42 +0000

    Test clicking add to cart button

--- src/pages/HomePage/Product/Product.test.jsx
+++ src/pages/HomePage/Product/Product.test.jsx
@@ -1,9 +1,14 @@
 import { it, expect, describe, vi } from 'vitest';
 import { render, screen } from '@testing-library/react';
 import { Product } from './Product.jsx';
+import { userEvent } from '@testing-library/user-event'
 import { fullURL } from '../../../api/api.jsx';
 
-describe('Product Component', () => {
+import { api } from '../../../api/api.jsx';
+
+vi.mock('../../../api/api.jsx')
+
+describe('Product Component',  () => {
   it('displays the product details correctly', () => {
     const product = {
       "id": "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
@@ -22,8 +27,36 @@ describe('Product Component', () => {
 
     expect(screen.getByText('Black and Gray Athletic Cotton Socks - 6 Pairs')).toBeInTheDocument();
     expect(screen.getByText('$10.90')).toBeInTheDocument();
-    expect(screen.getByTestId('product-image')).toHaveAttribute('src','images/products/athletic-cotton-socks-6-pairs.jpg');
-    expect(screen.getByTestId('product-rating-stars')).toHaveAttribute('src',`${fullURL}/images/ratings/rating-${product.rating.stars * 10}.png`);
+    expect(screen.getByTestId('product-image')).toHaveAttribute('src', 'images/products/athletic-cotton-socks-6-pairs.jpg');
+    expect(screen.getByTestId('product-rating-stars')).toHaveAttribute('src', `${fullURL}/images/ratings/rating-${product.rating.stars * 10}.png`);
     expect(screen.getByText('87')).toBeInTheDocument();
   })
+
+  it('adds a product to the cart', async () => {
+    const product = {
+      "id": "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+      "image": "images/products/athletic-cotton-socks-6-pairs.jpg",
+      "name": "Black and Gray Athletic Cotton Socks - 6 Pairs",
+      "rating": {
+        "stars": 4.5,
+        "count": 87
+      },
+      "priceCents": 1090,
+      "keywords": ["socks", "sports", "apparel"]
+    }
+
+    const loadCart = vi.fn()
+    render(<Product product={product} loadCart={loadCart} />);
+
+    const user = userEvent.setup();
+    await user.click(screen.getByTestId('add-to-cart-button'));
+    expect(api.post).toHaveBeenCalledWith(
+      `${fullURL}/api/cart/register`,
+      {
+        productID: product.id,
+        quantity: 1
+      }
+    );
+    expect(loadCart).toHaveBeenCalled();
+  });
 });
